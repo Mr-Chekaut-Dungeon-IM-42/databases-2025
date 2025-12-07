091b50ff5fa6869667521bdc611ff5f3450c1040
theammir <the@ammir.dev>
2025-12-07 23:50:48 +0200

    docs(6): update readme

--- lab6/src/db/session.py
+++ lab6/src/db/session.py
@@ -5,7 +5,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
-DB_URL = "postgresql://admin:password@localhost:5432/db_labs"
+DB_URL = os.environ.get("DB_URL", "postgresql://admin:password@localhost:5432/db_labs")
 
 # future=True is default in SQLAlchemy 2.x create_engine
 engine = create_engine(DB_URL, future=True)
--- lab6/src/main.py
+++ lab6/src/main.py
@@ -1,17 +1,6 @@
-from sqlalchemy import insert, select
-from sqlalchemy.orm import Session
-
-from db.models import User
-from db.session import get_session
-
-
 def main():
     print("Hi Andrii")
 
 
-def insert_test_data(db: Session) -> None:
-    pass
-
-
 if __name__ == "__main__":
-    db = get_session()
+    main()
